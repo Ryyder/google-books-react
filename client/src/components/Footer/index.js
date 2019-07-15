b954064cfdf7f7b0a37a3563602aec4a40f4a709
Ryyder <brucecho82@hotmail.com>
2019-07-14 18:06:16 -0700

    updated server

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const Footer = () => (
+	<footer className = "page-footer">
+		<div className = "container-fluid text-center">
+			<p>Created using React and Mongo</p>
+		</div>	
+	</footer>
+);
+
+export default Footer;
